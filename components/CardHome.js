79cc86c47a39dcb90ae5b6990d3d85d302ecaeac
anutacom <106591390+anutacom@users.noreply.github.com>
2023-02-02 16:34:30 +0500

    Change Header and other

---
+++ components/CardHome.js
@@ -0,0 +1,30 @@
+import * as React from "react";
+import Box from "@mui/material/Box";
+import Card from "@mui/material/Card";
+import CardActions from "@mui/material/CardActions";
+import CardContent from "@mui/material/CardContent";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
+import { Grid } from "@mui/material";
+
+
+
+export default function CardHome({kind, text, buttonText, handleSetCreateCampaign}) {
+  return (
+    <Card sx={{ minWidth: 275 }}>
+      <Grid container direction="column" alignItems="center">
+        <Grid item>
+          <CardContent>
+            <Typography sx={{ fontSize: 24 }}>{kind}</Typography>
+            <Typography variant="body2">{text}</Typography>
+          </CardContent>
+        </Grid>
+        <Grid item>
+          <CardActions>
+            <Button size="small" onClick={() => handleSetCreateCampaign()}>{buttonText}</Button>
+          </CardActions>
+        </Grid>
+      </Grid>
+    </Card>
+  );
+}
