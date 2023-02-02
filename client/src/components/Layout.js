41b07dd131387ccc2e3a9245363668649f50212c
Каримов Руслан <rmarakarimov@sberbank.ru>
2023-02-02 16:23:22 +0300

    navigate to Search screen

--- client/src/components/Layout.js
+++ client/src/components/Layout.js
@@ -1,32 +1,34 @@
-import { Container, Grid } from "@mui/material";
-import React, { useState } from "react";
+import {Container, Grid} from "@mui/material";
+import React, {useState} from "react";
 import CardHome from "./CardHome";
 import CreateCompany from "./CreateCampaign";
+import Search from "./Search&Donate";
 
-const Layout = ({ handleSetCreateCampaign, screen }) => {
+const Layout = ({handleSetCreateCampaign, handleSetDonateCryptoScreen, screen}) => {
 
-  return (
-    screen.isHome ?
-    <Container>
-      <Grid container justifyContent="center" spacing={6}>
-        <Grid item xs={4}>
-          <CardHome
-            handleSetCreateCampaign={handleSetCreateCampaign}
-            kind="Create campaign"
-            text="Somthing text"
-            buttonText="More"
-          />
-        </Grid>
-        <Grid item xs={4}>
-          <CardHome
-            kind="Donate crypto"
-            text="Somthing text"
-            buttonText="More"
-          />
-        </Grid>
-      </Grid>
-    </Container> : 
-    screen.isCreateCampaign ? <CreateCompany/> : null
-  );
+    return (
+        screen.isHome ?
+            <Container>
+                <Grid container justifyContent="center" spacing={6}>
+                    <Grid item xs={4}>
+                        <CardHome
+                            handleSetScreen={handleSetCreateCampaign}
+                            kind="Create campaign"
+                            text="Somthing text"
+                            buttonText="More"
+                        />
+                    </Grid>
+                    <Grid item xs={4}>
+                        <CardHome
+                            handleSetScreen={handleSetDonateCryptoScreen}
+                            kind="Donate crypto"
+                            text="Somthing text"
+                            buttonText="More"
+                        />
+                    </Grid>
+                </Grid>
+            </Container> :
+            screen.isCreateCampaign ? <CreateCompany/> : <Search/>
+    );
 };
 export default Layout;
