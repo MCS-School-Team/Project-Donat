110e6566fb0aa34f1b4d4b8a044a9dbe4571375d
anutacom <106591390+anutacom@users.noreply.github.com>
2023-02-19 21:45:09 +0500

    Change Carousel and button Connect (#30)
    * Add Connect MetaMask and refact Button
    
    * Rename ethereum2/Campaing.sol to ethereum/Campaing.sol
    
    * Rename ethereum2/abi.json to ethereum/src/abi.json
    
    * Rename ethereum2/Сampaing.js to ethereum/src/Сampaing.js
    
    * Rename ethereum/Campaing.sol to ethereum/contracts/Campaing.sol
    
    * Rename ethereum/src/abi.json to client/src/abi.json
    
    * Rename ethereum/src/Сampaing.js to client/src/Сampaing.js
    
    * Rename ethereum/contracts/CrowdFunding_v1.sol to ethereum2/CrowdFunding_v1.sol
    
    * Rename ethereum/contracts/ERC20.sol to ethereum2/ERC20.sol
    
    * Rename ethereum/contracts/IERC20.sol to ethereum2/IERC20.sol
    
    * Rename ethereum/contracts/JuiceBox.sol to ethereum2/JuiceBox.sol
    
    * Rename ethereum/contracts/SafeTransferLib.sol to ethereum2/SafeTransferLib.sol
    
    * Rename ethereum/contracts/remix-compiler.config.js to ethereum2/remix-compiler.config.js
    
    * Delete TokenSell_metadata.json
    
    * Delete TokenSell.json
    
    * Delete IERC20_metadata.json
    
    * Delete IERC20.json
    
    * Delete ERC20_metadata.json
    
    * Delete ERC20.json
    
    * Delete Crowd_metadata.json
    
    * Delete Crowd.json
    
    * Delete 082a547285a58eaab0fb219fb7d66e78.json
    
    * Delete 41726430fa7d14601161b3cf2d34bbbc.json
    
    * Delete 5721d010c1ac31081f93c97b9e2b366b.json
    
    * Delete 9f33a348d9c25aebca3d1aa0fcff3c83.json
    
    * Delete e620686e349dc7c3878baef85b7ff2c0.json
    
    * Delete f2a83b658eeb3e7a6da73de971165fda.json
    
    * Adding MetaMask
    
    * Delete remix-compiler.config.js
    
    * Delete SafeTransferLib.sol
    
    * Delete JuiceBox.sol
    
    * Delete IERC20.sol
    
    * Delete ERC20.sol
    
    * Delete CrowdFunding_v1.sol
    
    * Add 1/2 Carousel
    
    * Add Carousel and change button Connect
    
    * Change Campaing
    
    * Add CampaingsArray
    
    * Add CampaingsArray
    
    * Update GetСampaings.js
    
    * Change Carousel and button Connect

--- client/src/components/Button.js
+++ client/src/components/Button.js
@@ -1,4 +1,4 @@
-const Button = ({ buttonStyle, type, text, onClick,disabled}) => {
+const Button = ({ buttonStyle, type, text, onClick,disabled, image}) => {
   return (
     <div className="p-1">
       <button
@@ -10,12 +10,12 @@ const Button = ({ buttonStyle, type, text, onClick,disabled}) => {
         onClick={onClick}
         disabled ={disabled}
       >
-        {text}
+        {image}{text}
       </button>
     </div>
   );
 };
 const roundedButtonConnectWallet =
   " min-w-xs max-w-xs transition ease-in-out delay-150  bg-blue text-light-brown text-ellipsis overflow-hidden ... font-Chewy text-xl p-4 rounded-3xl hover:bg-light-blue hover:-translate-y-1 hover:scale-110 duration-300 ";
-const desabledButton = " bg-transparent border: none !important  text-brown font-Chewy text-xl font-black "
+const desabledButton = " bg-transparent border: none !important  text-yellow font-Chewy text-xl  "
 export default Button;
--- client/src/components/GetСampaings.js
+++ client/src/components/GetСampaings.js
@@ -1,24 +1,46 @@
 import { useState, useEffect } from "react";
 import campaingFactory from "../data/campaingFactory";
+import { Contract, ethers } from "ethers";
+import abi from "../abi.json";
 
 const GetCampaings = () => {
-
-  const [campaings, setCampaings] = useState();
-
+  const [name, setName] = useState([]);
+  const [address, setAddress] = useState([]);
+  const [loader, setLoader] = useState("true");
+  let provider = new ethers.BrowserProvider(window.ethereum);
   useEffect(() => {
     (async () => {
-      try{
-      const campaingsArray = await campaingFactory.campaingsArray();
-      setCampaings(campaingsArray)
-      console.log("campaingsArray:", campaingsArray);
-    } 
-    catch(error){
-      console.error(error)
+      setAddress(await campaingFactory.campaingsArray());
+    })();
+  }, []);
+  const comp = address.map((adr) => new Contract(`${adr}`, abi, provider));
+  useEffect(() => {
+    try {
+      (async () => {
+        let proper = [];
+        for (let i = 0; i < comp.length; i++) {
+          let por = {
+            name:await comp[i].name()
+          }
+          proper.push(por);
+        }
+        setName(proper);
+      })();
+    } catch (err) {
+      console.error(err);
+    } finally {
+      setLoader(false);
     }
-
-  
-  })();
-},[])
-
+  }, [address]);
+console.log(name)
+  return (
+    <>
+   
+        {name.map((item)=> 
+        <li >{item.name}</li>)}
+   
+    </>
+  );
 };
+
 export default GetCampaings;
