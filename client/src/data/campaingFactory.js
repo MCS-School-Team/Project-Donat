2bb4c4c6f53cb6e839304c5150e928d3f3976f36
anutacom <106591390+anutacom@users.noreply.github.com>
2023-02-20 11:10:13 +0500

    Change Contract and ABI (#32)
    * Add Connect MetaMask and refact Button
    
    * Rename ethereum2/Campaing.sol to ethereum/Campaing.sol
    
    * Rename ethereum2/abi.json to ethereum/src/abi.json
    
    * Rename ethereum2/Сampaing.js to ethereum/src/Сampaing.js
    
    * Rename ethereum/Campaing.sol to ethereum/contracts/Campaing.sol
    
    * Rename ethereum/src/abi.json to client/src/abi.json
    
    * Rename ethereum/src/Сampaing.js to client/src/Сampaing.js
    
    * Rename ethereum/contracts/CrowdFunding_v1.sol to ethereum2/CrowdFunding_v1.sol
    
    * Rename ethereum/contracts/ERC20.sol to ethereum2/ERC20.sol
    
    * Rename ethereum/contracts/IERC20.sol to ethereum2/IERC20.sol
    
    * Rename ethereum/contracts/JuiceBox.sol to ethereum2/JuiceBox.sol
    
    * Rename ethereum/contracts/SafeTransferLib.sol to ethereum2/SafeTransferLib.sol
    
    * Rename ethereum/contracts/remix-compiler.config.js to ethereum2/remix-compiler.config.js
    
    * Delete TokenSell_metadata.json
    
    * Delete TokenSell.json
    
    * Delete IERC20_metadata.json
    
    * Delete IERC20.json
    
    * Delete ERC20_metadata.json
    
    * Delete ERC20.json
    
    * Delete Crowd_metadata.json
    
    * Delete Crowd.json
    
    * Delete 082a547285a58eaab0fb219fb7d66e78.json
    
    * Delete 41726430fa7d14601161b3cf2d34bbbc.json
    
    * Delete 5721d010c1ac31081f93c97b9e2b366b.json
    
    * Delete 9f33a348d9c25aebca3d1aa0fcff3c83.json
    
    * Delete e620686e349dc7c3878baef85b7ff2c0.json
    
    * Delete f2a83b658eeb3e7a6da73de971165fda.json
    
    * Adding MetaMask
    
    * Delete remix-compiler.config.js
    
    * Delete SafeTransferLib.sol
    
    * Delete JuiceBox.sol
    
    * Delete IERC20.sol
    
    * Delete ERC20.sol
    
    * Delete CrowdFunding_v1.sol
    
    * Add 1/2 Carousel
    
    * Add Carousel and change button Connect
    
    * Change Campaing
    
    * Add CampaingsArray
    
    * Add CampaingsArray
    
    * Update GetСampaings.js
    
    * Change Carousel and button Connect
    
    * Change Contract and ABI

--- client/src/data/campaingFactory.js
+++ client/src/data/campaingFactory.js
@@ -1,104 +1,408 @@
 import {ethers, Contract} from "ethers"
-const address = "0xf9674917BDA84778FbCd615F33D6e913A034cAB8";
+const address = "0x44a2E1fc598c6DbC2c77d2d058f95e28dB3d93DB";
 const provider = new ethers.BrowserProvider(window.ethereum);
 const abi = [
-  {
-    inputs: [
-      {
-        internalType: "uint256",
-        name: "",
-        type: "uint256",
-      },
-    ],
-    name: "campaings",
-    outputs: [
-      {
-        internalType: "address",
-        name: "",
-        type: "address",
-      },
-    ],
-    stateMutability: "view",
-    type: "function",
-  },
-  {
-    inputs: [],
-    name: "campaingsArray",
-    outputs: [
-      {
-        internalType: "address[]",
-        name: "",
-        type: "address[]",
-      },
-    ],
-    stateMutability: "view",
-    type: "function",
-  },
-  {
-    inputs: [],
-    name: "campaingsCount",
-    outputs: [
-      {
-        internalType: "uint256",
-        name: "",
-        type: "uint256",
-      },
-    ],
-    stateMutability: "view",
-    type: "function",
-  },
-  {
-    inputs: [
-      {
-        internalType: "string",
-        name: "_name",
-        type: "string",
-      },
-      {
-        internalType: "string",
-        name: "_description",
-        type: "string",
-      },
-      {
-        internalType: "uint256",
-        name: "_goal",
-        type: "uint256",
-      },
-    ],
-    name: "createCampaing",
-    outputs: [
-      {
-        internalType: "address",
-        name: "",
-        type: "address",
-      },
-    ],
-    stateMutability: "nonpayable",
-    type: "function",
-  },
-  {
-    inputs: [
-      {
-        internalType: "address",
-        name: "",
-        type: "address",
-      },
-    ],
-    name: "ownerToCampaing",
-    outputs: [
-      {
-        internalType: "address",
-        name: "",
-        type: "address",
-      },
-    ],
-    stateMutability: "view",
-    type: "function",
-  },
-];
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "donater",
+				"type": "address"
+			},
+			{
+				"internalType": "uint256",
+				"name": "value",
+				"type": "uint256"
+			}
+		],
+		"name": "addTokens",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"stateMutability": "nonpayable",
+		"type": "constructor"
+	},
+	{
+		"anonymous": false,
+		"inputs": [
+			{
+				"indexed": true,
+				"internalType": "address",
+				"name": "owner",
+				"type": "address"
+			},
+			{
+				"indexed": true,
+				"internalType": "address",
+				"name": "spender",
+				"type": "address"
+			},
+			{
+				"indexed": false,
+				"internalType": "uint256",
+				"name": "value",
+				"type": "uint256"
+			}
+		],
+		"name": "Approval",
+		"type": "event"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "spender",
+				"type": "address"
+			},
+			{
+				"internalType": "uint256",
+				"name": "amount",
+				"type": "uint256"
+			}
+		],
+		"name": "approve",
+		"outputs": [
+			{
+				"internalType": "bool",
+				"name": "",
+				"type": "bool"
+			}
+		],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "string",
+				"name": "_name",
+				"type": "string"
+			},
+			{
+				"internalType": "string",
+				"name": "_description",
+				"type": "string"
+			},
+			{
+				"internalType": "uint256",
+				"name": "_goal",
+				"type": "uint256"
+			},
+			{
+				"internalType": "string",
+				"name": "_video",
+				"type": "string"
+			},
+			{
+				"internalType": "string",
+				"name": "_site",
+				"type": "string"
+			}
+		],
+		"name": "createCampaing",
+		"outputs": [
+			{
+				"internalType": "address",
+				"name": "",
+				"type": "address"
+			}
+		],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "spender",
+				"type": "address"
+			},
+			{
+				"internalType": "uint256",
+				"name": "subtractedValue",
+				"type": "uint256"
+			}
+		],
+		"name": "decreaseAllowance",
+		"outputs": [
+			{
+				"internalType": "bool",
+				"name": "",
+				"type": "bool"
+			}
+		],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "spender",
+				"type": "address"
+			},
+			{
+				"internalType": "uint256",
+				"name": "addedValue",
+				"type": "uint256"
+			}
+		],
+		"name": "increaseAllowance",
+		"outputs": [
+			{
+				"internalType": "bool",
+				"name": "",
+				"type": "bool"
+			}
+		],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "to",
+				"type": "address"
+			},
+			{
+				"internalType": "uint256",
+				"name": "amount",
+				"type": "uint256"
+			}
+		],
+		"name": "transfer",
+		"outputs": [
+			{
+				"internalType": "bool",
+				"name": "",
+				"type": "bool"
+			}
+		],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"anonymous": false,
+		"inputs": [
+			{
+				"indexed": true,
+				"internalType": "address",
+				"name": "from",
+				"type": "address"
+			},
+			{
+				"indexed": true,
+				"internalType": "address",
+				"name": "to",
+				"type": "address"
+			},
+			{
+				"indexed": false,
+				"internalType": "uint256",
+				"name": "value",
+				"type": "uint256"
+			}
+		],
+		"name": "Transfer",
+		"type": "event"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "from",
+				"type": "address"
+			},
+			{
+				"internalType": "address",
+				"name": "to",
+				"type": "address"
+			},
+			{
+				"internalType": "uint256",
+				"name": "amount",
+				"type": "uint256"
+			}
+		],
+		"name": "transferFrom",
+		"outputs": [
+			{
+				"internalType": "bool",
+				"name": "",
+				"type": "bool"
+			}
+		],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "owner",
+				"type": "address"
+			},
+			{
+				"internalType": "address",
+				"name": "spender",
+				"type": "address"
+			}
+		],
+		"name": "allowance",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "account",
+				"type": "address"
+			}
+		],
+		"name": "balanceOf",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"name": "campaings",
+		"outputs": [
+			{
+				"internalType": "address",
+				"name": "",
+				"type": "address"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "campaingsArray",
+		"outputs": [
+			{
+				"internalType": "address[]",
+				"name": "",
+				"type": "address[]"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "campaingsCount",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "",
+				"type": "address"
+			}
+		],
+		"name": "campaingToOwner",
+		"outputs": [
+			{
+				"internalType": "address",
+				"name": "",
+				"type": "address"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "decimals",
+		"outputs": [
+			{
+				"internalType": "uint8",
+				"name": "",
+				"type": "uint8"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "name",
+		"outputs": [
+			{
+				"internalType": "string",
+				"name": "",
+				"type": "string"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "symbol",
+		"outputs": [
+			{
+				"internalType": "string",
+				"name": "",
+				"type": "string"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "totalSupply",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	}
+]
 const ethABI = [
   "function createCampaing(string memory  _name,string memory  _description, uint _goal) public returns (address)",
   "function campaingsArray() public view returns (address[] memory)",
+  "  function addTokens(address donater,uint value) external"
 ];
 const campaingFactory = new Contract (address, ethABI, provider)
 
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -14,7 +14,6 @@ module.exports = {
     'light-brown':'rgba(250, 244, 238, 1)',
     "light-brown-2": "rgba(177, 155, 142, 1)",
     "md-brown": "rgba(163, 99, 40, 1)",
-    "black": "#000000",
     'brown': "rgba(88, 27, 27, 1)",
     'white': '#fafafa',
     'white-r': '#ffffff',
@@ -22,24 +21,13 @@ module.exports = {
     'light-blue': 'rgba(16, 54, 111, 1)',
     'blue':'rgba(13, 7, 81, 1)',
     'red': '#ff0000',
-    'slate-900':"rgb(15 23 42)",
-     'yellow':'rgba(217,119,6)',
-     'slate-400': "rgb(148 163 184)",
-     'slate-500': "rgb(100 116 139)",
-      'slate-600': "rgb(71 85 105)",   
-      'slate-700': "rgb(51 65 85)" ,  
-      'slate-800': "rgb(30 41 59)",
-       'cayn': "rgb(8 145 178)"
+    'yellow':'rgba(217, 119, 6)'
+    
     },
     fontFamily:{
      Chewy:['Chewy', 'cursive']
     },
     extend: {
-      backgroundImage: {
-        'mount':"url('./images/mount.png')",
-        'mounti2':"url('./images/mounti4.jpg')",
-        'forest':"url('./images/forests.png')"
-      },
       width: {
         "960" : "1100px",
       },
