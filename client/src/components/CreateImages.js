525a10da90da1d6ad809301a38d5860c0a3bc550
anutacom <106591390+anutacom@users.noreply.github.com>
2023-02-22 09:31:38 +0500

    Change CreateImages and CreateCampaing (#34)
    * Add Connect MetaMask and refact Button
    
    * Rename ethereum2/Campaing.sol to ethereum/Campaing.sol
    
    * Rename ethereum2/abi.json to ethereum/src/abi.json
    
    * Rename ethereum2/Сampaing.js to ethereum/src/Сampaing.js
    
    * Rename ethereum/Campaing.sol to ethereum/contracts/Campaing.sol
    
    * Rename ethereum/src/abi.json to client/src/abi.json
    
    * Rename ethereum/src/Сampaing.js to client/src/Сampaing.js
    
    * Rename ethereum/contracts/CrowdFunding_v1.sol to ethereum2/CrowdFunding_v1.sol
    
    * Rename ethereum/contracts/ERC20.sol to ethereum2/ERC20.sol
    
    * Rename ethereum/contracts/IERC20.sol to ethereum2/IERC20.sol
    
    * Rename ethereum/contracts/JuiceBox.sol to ethereum2/JuiceBox.sol
    
    * Rename ethereum/contracts/SafeTransferLib.sol to ethereum2/SafeTransferLib.sol
    
    * Rename ethereum/contracts/remix-compiler.config.js to ethereum2/remix-compiler.config.js
    
    * Delete TokenSell_metadata.json
    
    * Delete TokenSell.json
    
    * Delete IERC20_metadata.json
    
    * Delete IERC20.json
    
    * Delete ERC20_metadata.json
    
    * Delete ERC20.json
    
    * Delete Crowd_metadata.json
    
    * Delete Crowd.json
    
    * Delete 082a547285a58eaab0fb219fb7d66e78.json
    
    * Delete 41726430fa7d14601161b3cf2d34bbbc.json
    
    * Delete 5721d010c1ac31081f93c97b9e2b366b.json
    
    * Delete 9f33a348d9c25aebca3d1aa0fcff3c83.json
    
    * Delete e620686e349dc7c3878baef85b7ff2c0.json
    
    * Delete f2a83b658eeb3e7a6da73de971165fda.json
    
    * Adding MetaMask
    
    * Delete remix-compiler.config.js
    
    * Delete SafeTransferLib.sol
    
    * Delete JuiceBox.sol
    
    * Delete IERC20.sol
    
    * Delete ERC20.sol
    
    * Delete CrowdFunding_v1.sol
    
    * Add 1/2 Carousel
    
    * Add Carousel and change button Connect
    
    * Change Campaing
    
    * Add CampaingsArray
    
    * Add CampaingsArray
    
    * Update GetСampaings.js
    
    * Change Carousel and button Connect
    
    * Change Contract and ABI
    
    * Add CreateImage
    
    * Add CreateImages
    
    * Change CreateImages

--- client/src/components/CreateImages.js
+++ client/src/components/CreateImages.js
@@ -1,56 +1,61 @@
-
-import {useState, useEffect} from 'react'
+import { useState } from "react"
+import axios from "axios"
+const JWT = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySW5mb3JtYXRpb24iOnsiaWQiOiI0OTllOTBkOS0wNWI4LTRkZDctYTkzZC1lZDFmMTI3NzVjZjkiLCJlbWFpbCI6InByb3Noa2FlZEBnbWFpbC5jb20iLCJlbWFpbF92ZXJpZmllZCI6dHJ1ZSwicGluX3BvbGljeSI6eyJyZWdpb25zIjpbeyJpZCI6IkZSQTEiLCJkZXNpcmVkUmVwbGljYXRpb25Db3VudCI6MX0seyJpZCI6Ik5ZQzEiLCJkZXNpcmVkUmVwbGljYXRpb25Db3VudCI6MX1dLCJ2ZXJzaW9uIjoxfSwibWZhX2VuYWJsZWQiOmZhbHNlLCJzdGF0dXMiOiJBQ1RJVkUifSwiYXV0aGVudGljYXRpb25UeXBlIjoic2NvcGVkS2V5Iiwic2NvcGVkS2V5S2V5IjoiM2MxMGIxNDg3ODg1MmE2NWU0ZDIiLCJzY29wZWRLZXlTZWNyZXQiOiIyMDExZGZkZmY3OGM5YTAxNTVkMWUzMjU1ODljZjRlNTA2MzMxMGYxMTJmYWJjYTFlYWMzMTNiNDNmMTM4NGI3IiwiaWF0IjoxNjc2OTc0MDY3fQ.usFAlJ1XAV1acjCD6aUseqMQUfNfu79UAjyGTaxzXv0'
 
 
 function CreateImages() {
 
-  const [fileImg, setFileImg] = useState(null);
 
-  const sendFileToIPFS = async (e) => {
-
-    if (fileImg) {
-        try {
-
-            const formData = new FormData();
-            formData.append("file", fileImg);
-            
-            const axios = require('axios') 
-            const resFile = await axios({
-                method: "post",
-                url: "https://api.pinata.cloud/pinning/pinFileToIPFS",
-                data: formData,
-                headers: {
-                    'pinata_api_key': `${process.env.REACT_APP_PINATA_API_KEY}`,
-                    'pinata_secret_api_key': `${process.env.REACT_APP_PINATA_API_SECRET}`,
-                    "Content-Type": "multipart/form-data"
-                },
-            });
-
-            const ImgHash = `ipfs://${resFile.data.IpfsHash}`;
-         console.log(ImgHash); 
-//Take a look at your Pinata Pinned section, you will see a new file added to you list.   
-
-
-
-        } catch (error) {
-            console.log("Error sending File to IPFS: ")
-            console.log(error)
-        }
-       
-    }
-}
-useEffect(() => {
-  console.log(fileImg)
-}, [fileImg])
-
-return(
-  <label className="mb-2 text-sm font-medium text-gray-900 dark:text-whith"
-                               htmlFor="goal">
-  <form onSubmit={sendFileToIPFS}>
-  <input type="file" onChange={(e) =>setFileImg(e.target.files[0])} required />
-  <button type='submit' >Save</button>            
-  </form>
-  </label>
-  )
-}
+  
+ 
+  
+    const [selectedFile, setSelectedFile] = useState();
+  
+    const changeHandler = (event) => {
+      setSelectedFile(event.target.files[0]);
+    };
+  
+    const handleSubmission = async() => {
+  
+      const formData = new FormData();
+      
+      formData.append('file', selectedFile)
+  
+      const metadata = JSON.stringify({
+        name: 'File name',
+      });
+      formData.append('pinataMetadata', metadata);
+      
+      const options = JSON.stringify({
+        cidVersion: 0,
+      })
+      formData.append('pinataOptions', options);
+  
+      try{
+        const res = await axios.post("https://api.pinata.cloud/pinning/pinFileToIPFS", formData, {
+          maxBodyLength: "Infinity",
+          headers: {
+            'Content-Type': `multipart/form-data; boundary=${formData._boundary}`,
+            'pinata_api_key': 'b9e07e7b816ab663af52',
+            'pinata_secret_api_key': 'a2919d46a9937f833fcd14d5755dec170595369743d503ea15baccd551948bb2',
+          
+          }
+        });
+        console.log(res.data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+  
+    return (
+      <>
+     
+      <label className="mb-2 text-sm font-medium text-gray-900 dark:text-whith">Choose File</label>
+      <div className = "flex flex-row">
+      <input  className="phone-number bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-black dark:focus:ring-blue-500 dark:focus:border-blue-500" type="file"  onChange={changeHandler}/>
+      <button className = "rounded-lg p-3 ml-1 bg-gray-300" onClick={handleSubmission}>Submit</button>
+      </div>
+      </>
+    )
+  }
   export default CreateImages
