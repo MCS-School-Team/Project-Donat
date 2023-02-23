5b87910a11878f0799d2dc739f0437133ac76391
anutacom <106591390+anutacom@users.noreply.github.com>
2023-02-23 18:18:12 +0500

    Update Search&Donate (#38)
    * Add Connect MetaMask and refact Button
    
    * Rename ethereum2/Campaing.sol to ethereum/Campaing.sol
    
    * Rename ethereum2/abi.json to ethereum/src/abi.json
    
    * Rename ethereum2/Сampaing.js to ethereum/src/Сampaing.js
    
    * Rename ethereum/Campaing.sol to ethereum/contracts/Campaing.sol
    
    * Rename ethereum/src/abi.json to client/src/abi.json
    
    * Rename ethereum/src/Сampaing.js to client/src/Сampaing.js
    
    * Rename ethereum/contracts/CrowdFunding_v1.sol to ethereum2/CrowdFunding_v1.sol
    
    * Rename ethereum/contracts/ERC20.sol to ethereum2/ERC20.sol
    
    * Rename ethereum/contracts/IERC20.sol to ethereum2/IERC20.sol
    
    * Rename ethereum/contracts/JuiceBox.sol to ethereum2/JuiceBox.sol
    
    * Rename ethereum/contracts/SafeTransferLib.sol to ethereum2/SafeTransferLib.sol
    
    * Rename ethereum/contracts/remix-compiler.config.js to ethereum2/remix-compiler.config.js
    
    * Delete TokenSell_metadata.json
    
    * Delete TokenSell.json
    
    * Delete IERC20_metadata.json
    
    * Delete IERC20.json
    
    * Delete ERC20_metadata.json
    
    * Delete ERC20.json
    
    * Delete Crowd_metadata.json
    
    * Delete Crowd.json
    
    * Delete 082a547285a58eaab0fb219fb7d66e78.json
    
    * Delete 41726430fa7d14601161b3cf2d34bbbc.json
    
    * Delete 5721d010c1ac31081f93c97b9e2b366b.json
    
    * Delete 9f33a348d9c25aebca3d1aa0fcff3c83.json
    
    * Delete e620686e349dc7c3878baef85b7ff2c0.json
    
    * Delete f2a83b658eeb3e7a6da73de971165fda.json
    
    * Adding MetaMask
    
    * Delete remix-compiler.config.js
    
    * Delete SafeTransferLib.sol
    
    * Delete JuiceBox.sol
    
    * Delete IERC20.sol
    
    * Delete ERC20.sol
    
    * Delete CrowdFunding_v1.sol
    
    * Add 1/2 Carousel
    
    * Add Carousel and change button Connect
    
    * Change Campaing
    
    * Add CampaingsArray
    
    * Add CampaingsArray
    
    * Update GetСampaings.js
    
    * Change Carousel and button Connect
    
    * Change Contract and ABI
    
    * Add CreateImage
    
    * Add CreateImages
    
    * Change CreateImages
    
    * Change CreateImages and CreateCampaing
    
    * Update CreateCampaing and CreateImages
    
    * Update Search&Donate.js

--- client/src/components/Search&Donate.js
+++ client/src/components/Search&Donate.js
@@ -1,151 +1,274 @@
 import React from "react";
-import { useState,useEffect } from "react";
-import Modal from "./Modal"
-import { compony } from "../data/ArrayCompony";
-import { ethers,Contract} from "ethers";
-import abi from "../abi.json"
+import { useState, useEffect } from "react";
+import Modal from "./Modal";
+import { ethers, Contract } from "ethers";
+import abi from "../abi.json";
 import campaingFactory from "../data/campaingFactory";
-import filterName from "./serachFunc";
-import photoCamp from "../images/mount.png"
+import photoCamp from "../images/mount.png";
 
+function Search() {
+  const [word, setWord] = useState();
+  const [fin, setFin] = useState(false);
+  const [findCategory, setCategory] = useState("All");
+  const [findCountry, setCountry] = useState("All");
+  const [items, setItems] = useState([]);
+  const [modalActive, setModalActive] = useState(false);
+  const [forModal, setForModal] = useState(items[0]);
+  const [searchName, setSearchName] = useState([]);
+  const [loader, setLoader] = useState(true);
+  const [address, setAddress] = useState([]);
+  const [addrr, setAddrr] = useState();
+  // Провайдер с подключением контракта..............................................
+  const provider = new ethers.BrowserProvider(window.ethereum);
+  //..................................................................................
+  // let address = ["0x59f496E5580B7dF7de7BFAAF629eBf88B9CD0a15","0x0cf20925394275b0B177813c88E1FB5E5DBA8922","0x894fc722Eac35af3d8e4D492C6f4b242ace6395D"] // Массив с адресами контрактов
+  useEffect(() => {
+    (async () => {
+      setAddress(await campaingFactory.campaingsArray());
+    })();
+  }, []);
+  const comp = address.map((adr) => new Contract(`${adr}`, abi, provider));
 
+  useEffect(() => {
+    try {
+      (async () => {
+        let proper = [];
+        for (let i = 0; i < comp.length; i++) {
+          let por = {
+            name: await comp[i].name(),
+            description: await comp[i].description(),
+            goal: await comp[i].goal(),
+            video: await comp[i].video(),
+            site: await comp[i].site(),
+            time: await comp[i].time(),
+          };
+          proper.push(por);
+        }
+        setItems(proper);
+      })();
+    } catch (err) {
+      console.error(err);
+    } finally {
+      setLoader(false);
+    }
+  }, [address]);
+  console.log(items);
 
-const lol = async() => {
-  return [await campaingFactory.name()]
-}
+  useEffect(() => {
+    if (items.length !== 0) {
+      let time = setTimeout(() => {
+        setLoader(false);
+      }, 2000);
+      return () => {
+        clearTimeout(time);
+      };
+    }
+  }, [items]);
+  const changeSearch = (e) => {
+    setWord()
+    const word = e.target.value;
+    setWord(word);
+  };
 
-
-export default function Search(){
-    const [findName, setFind] = useState("");
-    const [fin, setFin] = useState(false);
-    const [findCategory, setCategory] = useState("All");
-    const [findCountry, setCountry] = useState("All");
-    const [items, setItems] = useState(compony);
-    const [modalActive, setModalActive] = useState(false)
-    const [forModal ,setForModal]=useState(items[0])
-    const [name,setName] = useState([])
-    const [loader ,setLoader] = useState(true)
-    const [address ,setAddress]=useState([])
-    const [addrr,setAddrr] = useState()
-    // Провайдер с подключением контракта..............................................
-    let contractFctory;
-    const provider = new ethers.BrowserProvider(window.ethereum); 
-    contractFctory = campaingFactory
-    //..................................................................................
-
-
-    // let address = ["0x59f496E5580B7dF7de7BFAAF629eBf88B9CD0a15","0x0cf20925394275b0B177813c88E1FB5E5DBA8922","0x894fc722Eac35af3d8e4D492C6f4b242ace6395D"] // Массив с адресами контрактов 
-
-    //Получение адресов...............................................................
-    
-    useEffect(() => { 
-       (async () => {
-       setAddress(await contractFctory.campaingsArray()) 
-       })()  
-    },[])
-    const comp = address.map(adr => new Contract(`${adr}`,abi,provider))
-    useEffect(()=>{ 
-      try{
-       (async () => {
-       let proper = []
-       for(let i = 0;i<comp.length;i++ ){
-       let por = await comp[i].name()
-       proper.push(por)
-     } 
-     setName(proper)
-     })()
-      } catch(err){
-       console.error(err)
+  const handlesearchName = () => {
+       if (word) {
+      setSearchName([]);
+      const liter = word[0].toUpperCase() + word[0].slice(1);
+      for (let i = 0; i < items.length; i++) {
+        if (items[i].name.includes(liter)) {
+          searchName.push(items[i]);
+          console.log('searchName:',searchName);
+          console.log('word:', word)
+         
+        }
       }
-        
-     },[address])
-     useEffect(()=>{
-       if(name.length !== 0){
-      let time = setTimeout(()=>{
-        setLoader(false)
-      },2000)
-      return()=>{
-        clearTimeout(time)
-      }
-      }
-     },[name])
+    }
+  };
 
-    function getName(e) {
-      setFind(e.target.value);
-    }
-    function getCategory(e) {
-      setCategory(e.target.value);
-    }
-    function getCountry(e) {
-      setCountry(e.target.value);
-    }
-    
-    useEffect(() => {
-      const filteredName = filterName(findName, findCategory, findCountry, compony);
-      setItems(filteredName);
-    }, [fin]);
 
-    function handleSubmit(e) {
-      setFin(!fin);
-      e.preventDefault();
-    }
-    function modal(index = 0){
 
-        setModalActive(true)
-        setForModal(items[index])
-        setAddrr(address[index])// Тут получаем индекс из кнопки и передаём переменной addrr элемент массива address
-    }
-    if(loader === true){
-     return(
-      <div className="flex justify-center items-center h-[700px]">
-      <div className=" p-10 rounded-full animate-spin w-10 border border-dashed" />
-      </div>
-     ) 
-    }else if(loader === false){
-   return (
+  function getCategory(e) {
+    setCategory(e.target.value);
+  }
+  function getCountry(e) {
+    setCountry(e.target.value);
+  }
+
+  // useEffect(() => {
+  //   const filteredName = filterName(findName, findCategory, findCountry, compony);
+  //   setItems(filteredName);
+  // }, [fin]);
+
+  function handleSubmit(e) {
+    setFin(!fin);
+    e.preventDefault();
+  }
+  function modal(index = 0) {
+    setModalActive(true);
+    setForModal(items[index]);
+    setAddrr(address[index]); // Тут получаем индекс из кнопки и передаём переменной addrr элемент массива address
+  }
+  if(loader === true){
+  return (
     <>
-    {/* Тут в Modal мы передаёи пропсы ,соответственно address={addrr} это передача адреса */}
-    <Modal active={modalActive} setActive={setModalActive} items={forModal} address={addrr} /> 
-    <div className="flex  bg-cover bg-gradient-to-b from-light-brown z-20 to-light-brown-2 font-Chewy flex-col ">
-      <div className="bg-mounti2 bg-cover ">
-    <div className="flex  justify-center  p-16">
-    <form className=" w-3/5 opacity-90" onSubmit={handleSubmit}>
-        <input type="search" className="w-10/12 h-[46px] text-[20px] border border-solid border-gray-300 rounded-l-lg  indent-2  " placeholder="Search by name..." value={findName} onChange={getName} />
-        <input type="submit"className="bg-blue h-[48px] -ml-1 text-[20px] text-white border rounded-r-lg  w-2/12" value="Search" />
-        <div className="w-full flex justify-center">
-        <select className="w-1/3 p-1 rounded-lg mr-2  border border-solid indent-2 border-gray-300 bg-white  mt-2 text-[20px]" onChange={getCategory}>
-        <option value="All">Filter by category</option>
-        {/* {list2.map(list => <option key={list} >{list}</option>)} */}
-        </select>
-        <select className="w-1/3 border border-solid  bg-white-r rounded-lg indent-2 border-gray-300 mt-2 text-[20px]" onChange={getCountry}>
-          <option value="All">Country</option>
-          {/* {list.map(item => <option key={item} >{item}</option> )} */}
-        </select>
+           <div className="flex justify-center items-center h-[700px]">
+          <div className=" p-10 rounded-full animate-spin w-10 border border-dashed"></div>
         </div>
-      </form>
-    </div>
-    <div className=" flex  justify-center">
-        <ul className=" flex   w-[1250px]  ">
- <div className="  flex   w-full flex-wrap ">
-          {name.map((item, index) => (
-            <li key={index}>
-                <div className="flex opacity-90 bg-white-r items-center text-[25px] rounded-lg  p-9 ml-7 mt-3 mb-4 flex-col drop-shadow-xl">
-                <img src={photoCamp} alt="" className="h-[200px] w-[200px]"  />
-                {item}
-                <button onClick={()=>modal(index)} className="text-[17px] w-10/12 rounded-lg h-10 mt-4 text-white bg-blue">Learn more</button>
+        </>
+        )
+     
+    }else if((loader === false) && (!word)){
+      return (
+       <>
+        {/* Тут в Modal мы передаёи пропсы ,соответственно address={addrr} это передача адреса */}
+        <Modal active={modalActive} setActive={setModalActive} items={forModal} address={addrr} />
+
+        <div className="flex  bg-cover bg-gradient-to-b from-light-brown z-20 to-light-brown-2 font-Chewy flex-col ">
+          <div className="bg-mounti2 bg-cover ">
+            <div className="flex  justify-center  p-16">
+              <form className=" w-3/5 opacity-90" onSubmit={handleSubmit}>
+                <input
+                  type="search"
+                  className="w-10/12 h-[46px] text-[20px] border border-solid border-gray-300 rounded-l-lg  indent-2  "
+                  placeholder="Search by name..."
+                  onChange={changeSearch}
+                />
+
+                <button
+                  type="submit"
+                  className="bg-blue h-[48px] -ml-1 text-[20px] text-white border rounded-r-lg  w-2/12"
+                  onClick={handlesearchName}
+                >
+                  Search
+                </button>
+                
+                <div className="w-full flex justify-center">
+                  <select
+                    className="w-1/3 p-1 rounded-lg mr-2  border border-solid indent-2 border-gray-300 bg-white  mt-2 text-[20px]"
+                    onChange={getCategory}
+                  >
+                    <option value="All">Filter by category</option>
+                    {/* {list2.map(list => <option key={list} >{list}</option>)} */}
+                  </select>
+                  <select
+                    className="w-1/3 border border-solid  bg-white-r rounded-lg indent-2 border-gray-300 mt-2 text-[20px]"
+                    onChange={getCountry}
+                  >
+                    <option value="All">Country</option>
+                    {/* {list.map(item => <option key={item} >{item}</option> )} */}
+                  </select>
                 </div>
-                </li>
-          ))}
-  </div>
-        </ul>
-      </div>
-      </div>
-      </div> 
-    </>     
-    )
-          }           
+              </form>
+            </div>
+
+           
+              <div className=" flex  justify-center">
+                <ul className=" flex   w-[1250px]  ">
+                  <div className="  flex   w-full flex-wrap ">
+                    {items.map((item, index) => (
+                      <li key={index}>
+                        <div className="flex opacity-90 bg-white-r items-center text-[25px] rounded-lg  p-9 ml-7 mt-3 mb-4 flex-col drop-shadow-xl">
+                          <img
+                            src={photoCamp}
+                            alt=""
+                            className="h-[200px] w-[200px]"
+                          />
+                          {item.name}
+                          <button
+                            onClick={() => modal(index)}
+                            className="text-[17px] w-10/12 rounded-lg h-10 mt-4 text-white bg-blue"
+                          >
+                            Learn more
+                          </button>
+                        </div>
+                      </li>
+                    ))}
+                  </div>
+                </ul>
+              </div>
+            
+          </div>
+        </div>
+      
+    </>
+  );
 }
+else if(loader === false){
+  return (
+  <>
+   {/* Тут в Modal мы передаёи пропсы ,соответственно address={addrr} это передача адреса */}
+   <Modal active={modalActive} setActive={setModalActive} items={forModal} address={addrr} />
 
+   <div className="flex  bg-cover bg-gradient-to-b from-light-brown z-20 to-light-brown-2 font-Chewy flex-col ">
+     <div className="bg-mounti2 bg-cover ">
+       <div className="flex  justify-center  p-16">
+         <form className=" w-3/5 opacity-90" onSubmit={handleSubmit}>
+           <input
+             type="search"
+             className="w-10/12 h-[46px] text-[20px] border border-solid border-gray-300 rounded-l-lg  indent-2  "
+             placeholder="Search by name..."
+             onChange={changeSearch}
+           />
 
+           <button
+             type="submit"
+             className="bg-blue h-[48px] -ml-1 text-[20px] text-white border rounded-r-lg  w-2/12"
+             onClick={handlesearchName}
+           >
+             Search
+           </button>
+           
+           <div className="w-full flex justify-center">
+             <select
+               className="w-1/3 p-1 rounded-lg mr-2  border border-solid indent-2 border-gray-300 bg-white  mt-2 text-[20px]"
+               onChange={getCategory}
+             >
+               <option value="All">Filter by category</option>
+               {/* {list2.map(list => <option key={list} >{list}</option>)} */}
+             </select>
+             <select
+               className="w-1/3 border border-solid  bg-white-r rounded-lg indent-2 border-gray-300 mt-2 text-[20px]"
+               onChange={getCountry}
+             >
+               <option value="All">Country</option>
+               {/* {list.map(item => <option key={item} >{item}</option> )} */}
+             </select>
+           </div>
+         </form>
+       </div>
 
+      
+         <div className=" flex  justify-center">
+           <ul className=" flex   w-[1250px]  ">
+             <div className="  flex   w-full flex-wrap ">
+               {items.map((item, index) => (
+                 <li key={index}>
+                   <div className="flex opacity-90 bg-white-r items-center text-[25px] rounded-lg  p-9 ml-7 mt-3 mb-4 flex-col drop-shadow-xl">
+                     <img
+                       src={photoCamp}
+                       alt=""
+                       className="h-[200px] w-[200px]"
+                     />
+                     {searchName.name}
+                     <button
+                       onClick={() => modal(index)}
+                       className="text-[17px] w-10/12 rounded-lg h-10 mt-4 text-white bg-blue"
+                     >
+                       Learn more
+                     </button>
+                   </div>
+                 </li>
+               ))}
+             </div>
+           </ul>
+         </div>
+       
+     </div>
+   </div>
+ 
+</>
+);
 
+}
+}
+export default Search;
